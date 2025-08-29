c16062e2d5a247b94817a6f51fc1c00f04c46a5f
Tanim <abdullahtanim27@gmail.com>
2025-08-29 15:10:04 +0600

    solved the issue but need to fix the route issue

--- src/utils/axiosRequest.js
+++ src/utils/axiosRequest.js
@@ -1,14 +1,29 @@
 import axios from 'axios'
+import { getToken } from 'next-auth/jwt'
+import { getSession } from 'next-auth/react'
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL
 
 const axiosRequest = async ({ req, ...options }) => {
+	let token = null
+
+	if (typeof window !== 'undefined') {
+		// Client-side: Use getSession()
+		const session = await getSession()
+		token = session?.accessToken
+	} else if (req) {
+		// Server-side: Use getToken()
+		token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+	}
+
+	const headers = token ? { Authorization: `Bearer ${token}` } : {}
+
 	const onSuccess = (res) => res.data
 	const onError = (err) => {
 		throw err.response?.data || 'An error occurred'
 	}
 
-	return axios({ ...options })
+	return axios({ ...options, headers })
 		.then(onSuccess)
 		.catch(onError)
 }
